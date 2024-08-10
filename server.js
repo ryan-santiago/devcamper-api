c4e9c80587c2e3883cc50e6f2668579570f61376
Ryan Santiago <ryan.santiago.ict@gmail.com>
2024-08-10 18:02:27 +0800

    adding route and controller

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const dotenv = require('dotenv')
 
+// Routes files
+const bootcamps = require('./routes/bootcamps')
+
 //Load env vars
 dotenv.config({path: './config/config.env'})
 
 const app = express()
+
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps)
+
+
 const PORT = process.env.PORT || 5000
 
 app.listen(
