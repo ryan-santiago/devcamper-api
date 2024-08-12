f9f617e9b8c462c4fff6e61d0093d320946beeb8
Ryan Santiago <ryan.santiago.ict@gmail.com>
2024-08-13 00:29:35 +0800

    adding simple CRUD and errorHandler fn

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
-const connectDB = require('./config/db')
 const colors = require('colors')
+const errorHandler = require('./middleware/error')
+const connectDB = require('./config/db')
 
 // Load env vars
 dotenv.config({path: './config/config.env'})
@@ -15,6 +16,9 @@ const bootcamps = require('./routes/bootcamps')
 
 const app = express()
 
+// Body parser
+app.use(express.json())
+
 // Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
@@ -22,6 +26,8 @@ if (process.env.NODE_ENV === 'development') {
 
 // Mount Routers
 app.use('/api/v1/bootcamps', bootcamps)
+
+app.use(errorHandler)
 
 
 const PORT = process.env.PORT || 5000
